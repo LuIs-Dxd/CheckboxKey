90519e60678cdada7835b90ce611fa0623e760ac
LuIs-Dxd <45068622+LuIs-Dxd@users.noreply.github.com>
2023-05-03 02:11:00 -0400

    Update CheckBox.js

--- CheckBoxjs/CheckBox.js
+++ CheckBoxjs/CheckBox.js
@@ -1,7 +1,7 @@
 javascript:(function() {
     function AccionKey(event) {
         const tecla = '.'; // <--- Aqui escribes tu letra favorita. | Write your fav letter | Delete this comment
-        var checkbox = document.getElementById("miCheckbox"); // <--- Despues escribes el ID del checkbox. | Write the checkbox id (you can use f12).
+        var checkbox = document.getElementById("myCheckbox"); // <--- Despues escribes el ID del checkbox. | Write the checkbox id (you can use f12).| Delete this comment
         if (event.key.toUpperCase() === tecla.toUpperCase()) {
             checkbox.checked = !checkbox.checked;
             var changeEvent = new Event('change');
